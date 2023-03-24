d1efe1edf6fb9071c30d685b6965a5ee7f36ab8e
Fernando Serpa <serpaef@outlook.com>
2023-03-24 11:22:29 -0300

    refactor: moving sockets instructions to it's own directory

--- index.js
+++ index.js
@@ -10,16 +10,7 @@ const io = require('socket.io')(http, {
     }
 });
 
-io.on('connection', (socket) => {
-    console.log(`User connected. ID ${socket.id}`);
-
-    socket.emit('welcome', 'Hi, welcome to the test of sending messages from back-end to front-end!');
-    
-    socket.on('ping', () => {
-        console.log(`${socket.id} sent a ping.`);
-        io.emit('pong', `${socket.id} sent a ping`);
-    });
-});
+require('./sockets/ping')(io);
 
 app.get('/', (_req, res) => {
     res.sendFile(__dirname + '/index.html');
